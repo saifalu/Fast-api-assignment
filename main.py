f7c804bf7451f778852f51776b638b0e6906c9b2
Syed Saif Ali <syedsaifali214@gmail.com>
2024-06-27 13:38:37 +0530

    ip changed

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+# main.py
+
 from fastapi import FastAPI
-import uvicorn
 import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message" : "Hello World!"}
+    return {"message": "Hello, Azure Web Apps!"}
 
-if __name__=='__main__':
+if __name__ == "__main__":
+    import uvicorn
+
+    # Get port from environment variable or use default (8000)
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
